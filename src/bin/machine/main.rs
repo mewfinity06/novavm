3b3a9cc4bbc54f43b3be4ab4653a9ead867aa2b1
Ashton Sickler <ashton.sickler@icloud.com>
2025-03-03 18:43:48 -0600

    added get contents path

--- src/bin/machine/main.rs
+++ src/bin/machine/main.rs
@@ -1,17 +1,28 @@
+use std::{
+    env,
+    fs::File,
+    io::{self, BufRead},
+};
+
 use novavm::Machine;
 
 fn main() -> Result<(), String> {
+    let args: Vec<_> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage {:?} [file].proj", env::current_exe());
+        std::process::exit(1);
+    }
+
+    let file_path = args.get(1).unwrap();
+
+    let data = get_data(file_path)?;
+
     let mut machine = Machine::new();
 
-    machine.set_data(&[
-        0x50, 16, 1,      // ADD 16 1 -> 17
-        0x61, 1,          // POP A
-        0x51, 0x53, 0x6,  // SUB 0x53, 0x6 -> 0x4D
-        0x61, 2,          // POP B
-        0x60, 3           // PUSH C
-    ]);
+    machine.set_data(&data);
 
-    // machine.self.registers[r as usize];print_data();
+    machine.print_data();
 
     while !machine.halt {
         machine.print_state();
@@ -19,4 +30,38 @@ fn main() -> Result<(), String> {
     }
 
     Ok(())
+}
+
+fn get_data(file_path: &str) -> Result<Vec<u8>, String> {
+    let file = match File::open(file_path) {
+        Ok(f) => f,
+        Err(_) => return Err(format!("could not read {}", file_path)),
+    };
+
+    let reader = io::BufReader::new(file);
+    let mut result = Vec::new();
+
+    for line in reader.lines() {
+        let line = line.map_err(|_| "could not read line".to_string())?;
+        let line = line.trim();
+        if line.is_empty() {
+            continue;
+        }
+
+        if line.starts_with("0x") {
+            let value = u16::from_str_radix(&line[2..], 16)
+                .map_err(|_| format!("could not parse hex number: {}", line))?;
+            let high_byte = (value >> 8) as u8; // Extract the high 8 bits
+            let low_byte = (value & 0xFF) as u8; // Extract the low 8 bits
+
+            if high_byte != 0x00 {
+                result.push(high_byte);
+            }
+            result.push(low_byte);
+        } else {
+            return Err(format!("invalid format: {}", line));
+        }
+    }
+
+    Ok(result)
 }
