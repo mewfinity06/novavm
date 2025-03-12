c306cdda4a3713be86a58627613f3e453d509103
Ashton Sickler <ashton.sickler@icloud.com>
2025-03-11 23:53:04 -0500

    FIXME: Fix preprocessor so that it reads 1 as Base10 and not errors

--- asm/print.asm
+++ asm/print.asm
@@ -1,7 +1,7 @@
 ; Write to standard out from DATA_SECTION_START+0..DATA_SECTION_START+20
-SYSCALL WRITE 1 0 20
+SYSCALL WRITE $1 $0 $20
 ; Write to standard out from DATA_SECTION_START+22..DATA_SECTION_START+59
-SYSCALL WRITE 1 22 59
+SYSCALL WRITE $1 $22 $59
 
 [[DATA]] "This is a print line!\n"
 [[DATA]] "Let's try some escaped \"characters\"\n"
